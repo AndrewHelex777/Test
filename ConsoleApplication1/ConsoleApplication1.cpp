ce1bc9bde51ec5c695050c2995cbf469eae0cf4c
Andrew <andrew@gmail.com>
2026-05-17 11:33:52 +0300

    Added print good day

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,9 +1,12 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+void print()
+{
+    cout << "Good day!" << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
 }
